b53c79b38a84036d1ee3d313a1ab72f294d8a2c9
Markus Flodmark <Markus@markuss-air-2.lan>
2017-02-19 17:49:30 +0100

    fixed populated data

--- SingleViewAppSwiftTemplate/ViewController.swift
+++ SingleViewAppSwiftTemplate/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: Alerts
     func alertAreaAccess() {
         let alert = UIAlertController(title: "Area access", message: "Not allowed", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
@@ -38,6 +39,64 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    // MARK: Buttons
+    
+    //Main row
+    @IBOutlet weak var employeeButton: UIButton!
+    @IBOutlet weak var managerButton: UIButton!
+    @IBOutlet weak var geustButton: UIButton!
+    @IBOutlet weak var vendorButton: UIButton!
+    
+    // Second Row
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    // Check Entrant Type
+    @IBAction func checkEntrantType(_ sender: UIButton) {
+    }
+
+    
+    // Check Entrant Sub Type
+    @IBAction func checkEntrantSubType(_ sender: UIButton) {
+    }
+    
+    // Generate Pass
+    
+    @IBAction func generatePass(_ sender: Any) {
+    }
+    
+    // Populate Date
+    
+    @IBAction func populateData(_ sender: Any) {
+        dateOfBirth.text = "1988-11-19"
+        ssn.text = "1234512345"
+        projectNr.text = "1"
+        firstName.text = "Markus"
+        lastName.text = "Flodmark"
+        company.text = "Apple"
+        streetAddress.text = "1 Infinite Loop"
+        city.text = "Cupertino"
+        state.text = "California"
+        zipCode.text = "95014"
+    }
+    
+    
+    // MARK: TextFields
+    
+    @IBOutlet weak var dateOfBirth: UITextField!
+    @IBOutlet weak var ssn: UITextField!
+    @IBOutlet weak var projectNr: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var company: UITextField!
+    @IBOutlet weak var streetAddress: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    
 }
 
 
